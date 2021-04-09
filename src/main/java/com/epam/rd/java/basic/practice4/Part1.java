f9dcac06c1453689d6f7deb73fafa34693f019f7
Dimon <beregd2013@gmail.com>
2021-04-09 03:30:15 +0300

    ,

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -1,9 +1,77 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Part1 {
 
-    public static void main(String[] args) {
+    // getting input string from the file
+    public static String getInput(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            Scanner scanner = new Scanner(new File(fileName), "UTF-8");
+            while (scanner.hasNextLine()) {
+                sb.append(scanner.nextLine()).append(System.lineSeparator());
+            }
+            scanner.close();
+            return sb.toString().trim();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return sb.toString();
+    }
 
+    // deleting the first two characters of each word with the length of 4 and more characters
+    public static void convert() {
+        try (BufferedReader in = new BufferedReader(new FileReader("part1.txt"))) {
+
+            String line;
+            StringBuilder result = new StringBuilder();
+
+            while ((line = in.readLine()) != null) {
+                String[] wordsInLine = line.split("[.,!?\\-\\s\\n]+");
+
+                for (String string : wordsInLine) {
+                    if (isLongerThanFour(string) && defineLocale(string) == "latn") {
+                        result.append(string.substring(2) + " ");
+                    } else if (isLongerThanFour(string) && defineLocale(string) == "cyrl") {
+                        result.append(string.substring(4) + " ");
+                    } else {
+                        result.append(string + " ");
+                    }
+                }
+            }
+
+            System.out.println(result);
+        }catch(IOException e){
+            e.getMessage();
+        }
+    }
+
+    // checking for right length of current word
+    public static boolean isLongerThanFour(String string){
+        return string.length() >= 4;
+    }
+
+    // define language of input word(one of cyrillic of latin languages)
+    private static String defineLocale(String string) {
+        char ch = string.charAt(0);
+
+        if (Character.isAlphabetic(ch)) {
+            if (Character.UnicodeBlock.of(ch).equals(Character.UnicodeBlock.CYRILLIC)) {
+                return "cyrl";
+            } else if (Character.UnicodeBlock.of(ch).equals(Character.UnicodeBlock.BASIC_LATIN)){
+                return "latn";
+            }
+        }
+        return "none";
+    }
+
+    public static void main(String[] args){
+        Part1.convert();
     }
 
 }
--- src/main/java/com/epam/rd/java/basic/practice4/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part2.java
@@ -1,9 +1,101 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.io.*;
+import java.security.SecureRandom;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Part2 {
+
+    public static final File FILE_1 = new File("part2.txt");
+    public static final File FILE_2 = new File("part2_sorted.txt");
+    public static final String ERROR = "Ошибка";
+
 
     public static void main(String[] args) {
 
+        createSortedArray(createArray());
+
+        Logger log = Logger.getLogger(Part2.class.getName());
+
+        try (Scanner scanner = new Scanner(FILE_1);
+             Scanner scanner2 = new Scanner(FILE_2)) {
+
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+
+            sb.append("input ==> ");
+            while (scanner.hasNextInt()) {
+                sb.append(scanner.nextInt() + " ");
+            }
+
+            sb2.append("output ==> ");
+            while (scanner2.hasNextInt()) {
+                sb2.append(scanner2.nextInt() + " ");
+            }
+            System.out.println(sb.deleteCharAt(sb.length() - 1));
+            System.out.println(sb2.deleteCharAt(sb2.length() - 1));
+
+        } catch (IOException e) {
+            log.info(ERROR);
+
+        }
+
     }
 
+    public static int[] createArray() {
+        Logger log = Logger.getLogger(Part2.class.getName());
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("part2.txt"));) {
+
+            for (int i = 0; i < 10; i++) {
+                int a = new SecureRandom().nextInt(50);
+                bw.write(a + " ");
+            }
+
+        } catch (IOException e) {
+            log.info(ERROR);
+
+        }
+
+        int[] dataArray = new int[10];
+        try (Scanner scanner = new Scanner(FILE_1);) {
+
+            int i = 0;
+            while (scanner.hasNextInt()) {
+                dataArray[i] = scanner.nextInt();
+                i++;
+            }
+
+        } catch (IOException e) {
+            log.info(ERROR);
+        }
+        return dataArray;
+    }
+
+    public static void createSortedArray(int[] dataArray) {
+        Logger log = Logger.getLogger(Part2.class.getName());
+        for (int j = 0; j < dataArray.length; j++) {
+            for (int i = 1; i < dataArray.length; i++) {
+                if (dataArray[i] < dataArray[i - 1]) {
+                    int tmp = dataArray[i];
+                    dataArray[i] = dataArray[i - 1];
+                    dataArray[i - 1] = tmp;
+                }
+            }
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("part2_sorted.txt"))) {
+            for (int i = 0; i < 10; i++) {
+                bw.write(dataArray[i] + " ");
+            }
+
+        } catch (IOException e) {
+            log.info(ERROR);
+        }
+
+
+    }
 }
+
+
